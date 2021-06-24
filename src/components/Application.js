2893022cbfc10bf4bf3033c3109b03098d5f52ac
LeoTexx <LeoTexx@users.noreply.github.com>
2021-06-24 22:51:20 +0100

    privacysemppo

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Switch } from "react-router-dom";
 import SignIn from "../pages/Login";
 import SignUp from "../pages/Signup";
 import NewsPage from "../pages/NewsPage";
+import PrivacyPolicy from "../pages/PrivacyPolicy";
 import { UserContext } from "../provider/UserProvider";
 
 function Application() {
   const user = useContext(UserContext);
   return user ? (
-    <NewsPage />
+    <Router>
+      <Switch>
+        <PrivacyPolicy path="/privacy" exact />
+        <NewsPage />
+      </Switch>
+    </Router>
   ) : (
     <Router>
       <Switch>
